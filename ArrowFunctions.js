774e3ec7186e08801d64e82a5cf4fca476c1cbde
Diego Fernando Caviedes Camacho <diego_1996.c.c@hotmail.com>
2020-11-13 23:53:19 -0500

    Feat: Agreando formas de Arrow Function, y ArrowFunction Implicita

---
+++ ArrowFunctions.js
@@ -0,0 +1,46 @@
+var diego = {
+    nombre: 'diego',
+    apellido: 'caviedes',
+    edad: 24,
+    ingeniero: false,
+    cocinero: false,
+    cantante:   true,
+};
+//Magic Number
+const Mayoria_Edad = 18;
+
+/*function esMayorEdad(persona) {
+    return persona.edad >= Mayoria_Edad //valida la edad de la persona con el numero
+}*/
+
+//1 Forma
+//variable se le asigna la Funcion Anonima
+// const esMayorEdad = function(persona){
+//     return persona.edad >= Mayoria_Edad 
+// }
+
+//2 Forma Otra Forma ARROW FUNCTION sin () ----------
+// const esMayorEdad = persona => { //Cuando hay 1 solo param se le puede quitar los parentesis ()
+//     return persona.edad >= Mayoria_Edad 
+// }
+
+
+//3  FORMA
+//esto es lo mismo que arriba Arrow Function MAS ABREVIADO SIN () Y {}
+                                //ESTO ES LO QUE RETORNA
+const esMayorEdad = persona => persona.edad >= Mayoria_Edad ; 
+
+
+
+
+
+function imprimirSiEsMayorEdad(persona){
+    if  (persona.edad > 18)  {
+        //console.log(`${persona.nombre} es mayor de edad`)//Comillas invertidad para interpolar la var
+        if (esMayorEdad(persona)) {
+            console.log(`${persona.nombre} es mayor de edad`)//Comillas invertidad para interpolar la var
+        }
+    } else {
+        console.log(`${persona.nombre} es menor de edad`)
+    }
+}
