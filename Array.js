a0738bd75868f1691d5d6fa3b8b0fb636bf3c354
Diego Fernando Caviedes Camacho <diego_1996.c.c@hotmail.com>
2020-11-18 23:57:36 -0500

    Feat: Implementando el metodo Filter para los arrays

--- Array.js
+++ Array.js
@@ -34,12 +34,20 @@ var paula = {
     altura: 1.76
 }
 
-const esAlta = (persona) => {
-    return  persona.altura > 1.8
-}
+const esAlta = persona => persona.altura > 1.8
+//Es lo mismo de arriba
+const esAlta = ({altura}) => altura > 1.8
 
 var personas = [diego, alan, martin, dario, vicky, paula];
 
+//El filter recorre el Array Personas y le pasa cada 
+//una de las personas que estan dentro del array
+                                    //cada una de las personas
 var personasAltas = personas.filter(esAlta); 
+
+//Es lo mismo que lo de arriba pero conuna funcion Anonima
+// var personasAltas = personas.filter( function (persona) {
+//     return persona.altura > 1.8
+// })
 
 console.log(personasAltas);
