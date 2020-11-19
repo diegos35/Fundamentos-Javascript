a952f6c8026cfe69e1e1f74c5bfd5e0cbd81ffdc
Diego Fernando Caviedes Camacho <diego_1996.c.c@hotmail.com>
2020-11-18 23:34:36 -0500

    Feat: Filtrando array

---
+++ Array.js
@@ -0,0 +1,45 @@
+var diego = {
+    nombre: 'diego ',
+    apellido: 'fernando',
+    altura: 1.72
+}
+
+var alan = {
+    nombre: 'Alan',
+    apellido: 'Perez',
+    altura: 1.86
+}
+
+var martin = {
+    nombre: 'Martin',
+    apellido: 'Gomez',
+    altura: 1.85
+}
+
+var dario = {
+    nombre: 'Dario',
+    apellido: 'Juarez',
+    altura: 1.71
+}
+
+var vicky = {
+    nombre: 'Vicky',
+    apellido: 'Zapata',
+    altura: 1.56
+}
+
+var paula = {
+    nombre: 'Paula',
+    apellido: 'Barros',
+    altura: 1.76
+}
+
+const esAlta = (persona) => {
+    return  persona.altura > 1.8
+}
+
+var personas = [diego, alan, martin, dario, vicky, paula];
+
+var personasAltas = personas.filter(esAlta); 
+
+console.log(personasAltas);
