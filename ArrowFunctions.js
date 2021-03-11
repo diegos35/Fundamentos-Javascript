0e8ec99920dbdb45e412a6e7c3ec40b5bcf56362
Diego Fernando Caviedes Camacho <diego_1996.c.c@hotmail.com>
2021-03-11 01:55:38 -0500

    Promesas

--- ArrowFunctions.js
+++ ArrowFunctions.js
@@ -13,19 +13,19 @@ const Mayoria_Edad = 18;
     return persona.edad >= Mayoria_Edad //valida la edad de la persona con el numero
 }*/
 
-//1 Forma
+//1 Forma------------------------------------------------
 //variable se le asigna la Funcion Anonima
 const esMayorEdad = function(persona){
     return persona.edad >= Mayoria_Edad 
 }
 
-//2 Forma Otra Forma ARROW FUNCTION sin () ----------
+//2 Forma Otra Forma ARROW FUNCTION sin () --------------------------------
 const esMayorEdad = persona => { //Cuando hay 1 solo param se le puede quitar los parentesis ()
     return persona.edad >= Mayoria_Edad 
 }
 
 
-//3  FORMA
+//3  FORMA------------------------------------------------------------------
 //esto es lo mismo que arriba Arrow Function MAS ABREVIADO SIN () Y {}
                                 //ESTO ES LO QUE RETORNA
 //destructuramos el Objeto que pasamos por Param
--- OrdenCallbacksAsincronismo.js
+++ OrdenCallbacksAsincronismo.js
@@ -3,58 +3,28 @@ const PEOPLE_URL = 'people/:id'
 const opts = {crossDomain: true}  
 
 
-function obtnerPersonaje(id, callback){
-    console.log(id)
-    const url = `${API_URL}${PEOPLE_URL.replace(':id', id )}`
-    
-    $
-    .get(url, opts, callback)
-    .fail(function () {
-        console.log('sucedio un error no se pudo obtner el personaje ${id}')
-    })
-    /**como Arrow Funtion */
-    // $
-    // .get(url, opts, callback)
-    // .fail(() => {
-    //     console.log('sucedio un error no se pudo obtner el personaje ${id}')
-    // })
+function obtnerPersonaje (id) {
+  return new Promise((resolve, reject) => {
+    const url = `${API_URL}${PEOPLE_URL.replace(":id", id)}`
+    $.get(url, opts, function(data) {
+        resolve(data)
+        })
+       .fail(() => reject(id))
+  });
 }
 
+function onError(id) {
+    console.log(`sucedio un error al obterner personaje ${id}`)
+}
 
-
-/**
- * le pasamos 2 parametro el id que quermos obtener
- * y el Callback
-*/
-
-obtnerPersonaje(1, function({result}) { 
-
-    console.log(`Hola yo soy ${result.properties.name}`);
-
-    obtnerPersonaje(2, function(){
-        console.log(`Hola yo soy ${result.properties.name}`);
-
-        obtnerPersonaje(3, function() {
-            console.log(`Hola yo soy ${result.properties.name}`);
-
-            obtnerPersonaje(4, function(){
-                console.log(`Hola yo soy ${result.properties.name}`);
-                
-                obtnerPersonaje(5, function () {
-                    console.log(`Hola yo soy ${result.properties.name}`);
-
-                    obtnerPersonaje(6, function () {
-                        console.log(`Hola yo soy ${result.properties.name}`);
-                        
-                        obtnerPersonaje(7, function () {
-                            console.log(`Hola yo soy ${result.properties.name}`);
-                        })
-                    })
-                })
-            })
-        })        
+obtnerPersonaje(1)
+    .then(function (result) { 
+        console.log(`el persona 1 es ${result.properties.name}`)
     })
-})
-//obtnerPersonaje(2)
-//obtnerPersonaje(3)
+    .catch(onError)
+
+    // .catch(function (id) {
+    //     onError(id)
+    // } )
+
 
