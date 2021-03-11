304b3e9c7d9e6531686ba7721d8010824fa9aaf3
Diego Fernando Caviedes Camacho <diego_1996.c.c@hotmail.com>
2021-03-11 14:36:57 -0500

    Promesas encadenadas

--- OrdenCallbacksAsincronismo.js
+++ OrdenCallbacksAsincronismo.js
@@ -18,11 +18,33 @@ function onError(id) {
 }
 
 obtnerPersonaje(1)
-    .then(function (result) { 
-        console.log(`el persona 1 es ${result.properties.name}`)
+    .then( personaje1 => { //arrow function
+        console.log(`el persona 1 es ${personaje1.result.properties.name}`)
+        return obtnerPersonaje(2)
+    })
+    .then( personaje2 => {
+      console.log(`el persona 2 es ${personaje2.result.properties.name}`)
+      return obtnerPersonaje(3)
+    })
+    .then( personaje3 => {
+      console.log(`el persona 3 es ${personaje3.result.properties.name}`)
+      return obtnerPersonaje(5)
+    })
+    .then( personaje4 => {
+      console.log(`el persona 4 es ${personaje4.result.properties.name}`)
+      return obtnerPersonaje(5)
+    })
+    .then( personaje4 => {
+      console.log(`el persona 5 es ${personaje4.result.properties.name}`)
+      return obtnerPersonaje(6)
     })
     .catch(onError)
-
+    
+    /**
+     .then(function (result) { 
+        console.log(`el persona 1 es ${result.properties.name}`)
+    })
+     */
     // .catch(function (id) {
     //     onError(id)
     // } )
